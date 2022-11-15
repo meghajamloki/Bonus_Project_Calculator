ba06dde82b026ef417381720780c43fb301cdddc
navya <deekshithareddykallem21@gmail.com>
2022-11-15 13:50:44 -0600

    added a few changes

--- Calculator/src/com/Calculator.java
+++ Calculator/src/com/Calculator.java
@@ -48,8 +48,9 @@ public class Calculator {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//initializing frame
 		frame = new JFrame();
-		frame.setBounds(100, 100, 442, 521);
+		frame.setBounds(100, 100, 442, 521); //bounds for frame of calculator
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -58,7 +59,7 @@ public class Calculator {
 		lblNewLabel.setForeground(Color.BLUE);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel.setBounds(10, 92, 408, 34);
-		frame.getContentPane().add(lblNewLabel);
+		frame.getContentPane().add(lblNewLabel); //adding button to frame
 		
 		textField = new JTextField();
 		textField.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -66,9 +67,11 @@ public class Calculator {
 		textField.setEditable(false);
 		textField.setFont(new Font("Calibri", Font.BOLD, 32));
 		textField.setBounds(10, 10, 408, 73);
-		frame.getContentPane().add(textField);
+		frame.getContentPane().add(textField); //adding button to frame
 		textField.setColumns(10);
 		
+		
+		//button with 7 number
 		JButton btn7 = new JButton("7");
 		btn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -78,8 +81,9 @@ public class Calculator {
 		});
 		btn7.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn7.setBounds(10, 206, 85, 60);
-		frame.getContentPane().add(btn7);
+		frame.getContentPane().add(btn7); //adding button to frame
 		
+		//button with 4 number
 		JButton btn4 = new JButton("4");
 		btn4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -89,8 +93,9 @@ public class Calculator {
 		});
 		btn4.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn4.setBounds(10, 276, 85, 60);
-		frame.getContentPane().add(btn4);
+		frame.getContentPane().add(btn4); //adding button to frame
 		
+		//button with 1 number
 		JButton btn1 = new JButton("1");
 		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -100,8 +105,9 @@ public class Calculator {
 		});
 		btn1.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn1.setBounds(10, 346, 85, 60);
-		frame.getContentPane().add(btn1);
+		frame.getContentPane().add(btn1); //adding button to frame
 		
+		//button with 8 number
 		JButton btn8 = new JButton("8");
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -111,8 +117,9 @@ public class Calculator {
 		});
 		btn8.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn8.setBounds(105, 206, 85, 60);
-		frame.getContentPane().add(btn8);
+		frame.getContentPane().add(btn8); //adding button to frame
 		
+		//button with 5 number
 		JButton btn5 = new JButton("5");
 		btn5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -122,8 +129,9 @@ public class Calculator {
 		});
 		btn5.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn5.setBounds(105, 276, 85, 60);
-		frame.getContentPane().add(btn5);
+		frame.getContentPane().add(btn5); //adding button to frame
 		
+		//button with 2 number
 		JButton btn2 = new JButton("2");
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -133,8 +141,9 @@ public class Calculator {
 		});
 		btn2.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn2.setBounds(105, 346, 85, 60);
-		frame.getContentPane().add(btn2);
+		frame.getContentPane().add(btn2); //adding button to frame
 		
+		//button with 9 number
 		JButton btn9 = new JButton("9");
 		btn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -144,8 +153,9 @@ public class Calculator {
 		});
 		btn9.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn9.setBounds(200, 206, 85, 60);
-		frame.getContentPane().add(btn9);
+		frame.getContentPane().add(btn9); //adding button to frame
 		
+		//button with 6 number
 		JButton btn6 = new JButton("6");
 		btn6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -155,8 +165,9 @@ public class Calculator {
 		});
 		btn6.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn6.setBounds(200, 276, 85, 60);
-		frame.getContentPane().add(btn6);
-		
+		frame.getContentPane().add(btn6); //adding button to frame
+		 
+		//button with 3 number
 		JButton btn3 = new JButton("3");
 		btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -166,8 +177,9 @@ public class Calculator {
 		});
 		btn3.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn3.setBounds(200, 346, 85, 60);
-		frame.getContentPane().add(btn3);
+		frame.getContentPane().add(btn3); //adding button to frame
 		
+		//button with dot
 		JButton btndot = new JButton(".");
 		btndot.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -177,8 +189,9 @@ public class Calculator {
 		});
 		btndot.setFont(new Font("Calibri", Font.BOLD, 19));
 		btndot.setBounds(200, 413, 85, 60);
-		frame.getContentPane().add(btndot);
+		frame.getContentPane().add(btndot); //adding button to frame
 		
+		//button with 0 number
 		JButton btn0 = new JButton("0");
 		btn0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -188,8 +201,9 @@ public class Calculator {
 		});
 		btn0.setFont(new Font("Calibri", Font.BOLD, 19));
 		btn0.setBounds(10, 413, 180, 60);
-		frame.getContentPane().add(btn0);
+		frame.getContentPane().add(btn0); //adding button to frame
 		
+		//button with = sign
 		JButton btntEqual = new JButton("=");
 		btntEqual.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -226,8 +240,9 @@ public class Calculator {
 		});
 		btntEqual.setFont(new Font("Calibri", Font.BOLD, 19));
 		btntEqual.setBounds(295, 413, 127, 60);
-		frame.getContentPane().add(btntEqual);
+		frame.getContentPane().add(btntEqual); //adding button to frame
 		
+		//button with + sign
 		JButton btnAdd = new JButton("+");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -238,8 +253,9 @@ public class Calculator {
 		});
 		btnAdd.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnAdd.setBounds(295, 136, 127, 60);
-		frame.getContentPane().add(btnAdd);
+		frame.getContentPane().add(btnAdd); //adding button to frame
 		
+		//button with - sign
 		JButton btnSubtract = new JButton("-");
 		btnSubtract.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -250,8 +266,9 @@ public class Calculator {
 		});
 		btnSubtract.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnSubtract.setBounds(295, 276, 127, 60);
-		frame.getContentPane().add(btnSubtract);
+		frame.getContentPane().add(btnSubtract); //adding button to frame
 		
+		//button with * sign
 		JButton btnMultiply = new JButton("*");
 		btnMultiply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -262,8 +279,9 @@ public class Calculator {
 		});
 		btnMultiply.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnMultiply.setBounds(295, 346, 127, 60);
-		frame.getContentPane().add(btnMultiply);
+		frame.getContentPane().add(btnMultiply); //adding button to frame
 		
+		//button with / sign
 		JButton btnDivide = new JButton("/");
 		btnDivide.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -273,8 +291,9 @@ public class Calculator {
 		});
 		btnDivide.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnDivide.setBounds(295, 206, 127, 60);
-		frame.getContentPane().add(btnDivide);
+		frame.getContentPane().add(btnDivide); //adding button to frame
 		
+		//button with AC sign
 		JButton btnC = new JButton("AC");
 		btnC.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -284,8 +303,9 @@ public class Calculator {
 		});
 		btnC.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnC.setBounds(10, 136, 180, 60);
-		frame.getContentPane().add(btnC);
+		frame.getContentPane().add(btnC); //adding button to frame
 		
+		//button with C sign
 		JButton btnBack = new JButton("C");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -300,7 +320,7 @@ public class Calculator {
 		});
 		btnBack.setFont(new Font("Calibri", Font.BOLD, 19));
 		btnBack.setBounds(200, 136, 85, 60);
-		frame.getContentPane().add(btnBack);
+		frame.getContentPane().add(btnBack); //adding button to frame
 		
 	
 	}
